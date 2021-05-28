3caa3ae120d614a592e503efcc276c8542834fcb
AlessandraSchiavone <alessandraschiavone31@gmail.com>
2021-05-28 14:38:11 +0200

    aggiunta bonus

---
+++ src/components/Hero.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="jumbotron">
+        <img src="../assets/vuejumbo.png">
+    </div> 
+</template>
+
+<script>
+export default {
+    name:"Hero"
+}
+</script>
+
+<style>
+
+</style>
